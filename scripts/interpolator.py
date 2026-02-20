e0fb72463b951e0d52358a8e4ea61fdead1a4fed
nuko7055 <neko7055@gmail.com>
2026-02-20 20:45:48 +0800

    Update noise scheduler to use partial triangular interpolation for add_noise method

--- scripts/interpolator.py
+++ scripts/interpolator.py
@@ -47,17 +47,24 @@ def triangular_interpolate(self, sources, targets, timesteps, add_noise=False) :
     else:
         return mu_t, d_mu_t
 
-# enc-dec (need debug)
-# timesteps_normalize = timesteps.float() / noise_scheduler.num_train_timesteps
-# t_ = timesteps_normalize[:, None, None, None, None]
-# mu_coef = torch.cos(torch.pi * t_) ** 2
-# d_mu_coef = -torch.pi * torch.sin(2 * torch.pi * t_)
-# z_coef = torch.sin(torch.pi * t_) ** 2
-# d_z_coef = torch.pi * torch.sin(2 * torch.pi * t_)
-# mask = (t_ < 0.5).to(torch.float32)
-# images = src_images * mask + tar_images * (1 - mask)
-# mu_t = mu_coef * images
-# d_mu_t = d_mu_coef * images
-# noise = torch.randn_like(mu_t)
-# noisy_latent = mu_t + z_coef * noise
-# model_gt = d_mu_t + d_z_coef * noise
+def enc_dec_interpolate(self, sources, targets, timesteps, add_noise=True) :
+    assert add_noise, "enc-dec interpolation requires noise to be added, yet add_noise is set to False"
+    timepoints = timesteps.float() / self.num_train_timesteps
+    assert sources.shape == targets.shape
+    # expand timepoint to noise shape
+    if sources.ndim == 5:
+        timepoints = timepoints[..., None, None, None, None]
+    elif sources.ndim == 4:
+        timepoints = timepoints[..., None, None, None]
+    else:
+        raise ValueError(f"noise tensor has to be 4D or 5D tensor, yet got shape of {sources.shape}")
+    mu_coef = torch.cos(torch.pi * timepoints) ** 2
+    d_mu_coef = -torch.pi * torch.sin(2 * torch.pi * timepoints)
+    z_coef = torch.sin(torch.pi * timepoints) ** 2
+    d_z_coef = torch.pi * torch.sin(2 * torch.pi * timepoints)
+    mask = (timepoints < 0.5).to(torch.float32)
+    images = sources * mask + targets * (1 - mask)
+    mu_t = mu_coef * images
+    d_mu_t = d_mu_coef * images
+    noise = torch.randn_like(mu_t)
+    return mu_t + z_coef * noise, d_mu_t + d_z_coef * noise
